aa98c1f94c62714de5912fd469c6fbbf521af5c8
Gavin Pittson <gavinjp28@gmail.com>
2022-04-16 20:57:31 -0700

    sldc

--- sldc.js
+++ sldc.js
@@ -5,12 +5,14 @@
     Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
 */
 
-// Analysis of Requirements
-// Design
-// Implementation
-// Maintain
-// Planning
-// Testing & Integration
+// Analysis of Requirements       1. Planning
+// Design                         2. Analysis of Requirements 
+// Implementation                 3. Design
+// Maintain                       4. Implementation
+// Planning                       5. Testing & Integration
+// Testing & Integration          6. Maintain
+
+
 
 
 //////////////////PROBLEM 2////////////////////
@@ -19,3 +21,13 @@
     The first one is done for you. 
 */
 const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+
+const analysisOfRequirements = 'This is the second step, in which developers will asess what the nessessary requirements and functions of the software must be. "Requirements" can be defined as anything to specific funstions of the application, to needing the device to run the appliction on.'
+
+const Design = 'This is the third step, in which developers now begin to visually construct how the appliction will look and how it will function. Essentially laying down all the tools, materials, and constructing the blueprints before building.'
+
+const Implementation = 'This is the fourth step, in which the developers now begin to construct the software with all the nessessary tools gathered and parameters provided'
+
+const Testing = 'This is the fifth step, in which developers, with the now first intial completion of the software, begin to analyze the software to make sure it runs properly and to identify any bugs, errors, or defects. Contunine until all identifiable problems are fixed'
+
+const Maintain = " This is the sixth step, in which developers, with a now completed functioning software, must create ease of use for maintenance and support of the software, if a situations deems is nessessary along with the final installation "
